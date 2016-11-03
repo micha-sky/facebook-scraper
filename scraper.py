eafac0fce0898fa7f34fad45aa52a27a2b9711f4
micha <michalsky@env.kiev.ua>
2016-11-03 10:08:53 +0200

    Made it work with graph ids

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import json
 from facepy import GraphAPI
 
+
 login = str(sys.argv[1])
 password = str(sys.argv[2])
 target_id = str(sys.argv[3])
@@ -13,11 +14,10 @@ graph = GraphAPI(oauth_access_token)
 
 url = "https://m.facebook.com/login.php"
 
-# login to facebook
+#login to facebook
 br = mechanize.Browser()
 br.set_handle_robots(False)
-br.addheaders = [('User-agent',
-                  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/45.0.2454101')]
+br.addheaders = [('User-agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/45.0.2454101')]
 br.open(url)
 br.select_form(nr=0)
 br.form['email'] = login
@@ -26,13 +26,28 @@ br.submit()
 
 
 def get_user_info(user_id):
-    # fb_url = "https://www.facebook.com/{0}".format(user_id)
+    fb_url = "https://www.facebook.com/{0}".format(user_id)
     print "Processing user id: {0}".format(user_id)
-    fb_url = "https://www.facebook.com/profile.php?id={0}&sk=about".format(user_id)
+    # fb_url = "https://www.facebook.com/profile.php?id={0}&sk=about".format(user_id)
 
     br.open(fb_url)
     all_html = br.response().get_data()
     soup = BeautifulSoup(all_html, 'html.parser')
+    info = soup.find(type="application/ld+json")
+    if info is None:
+        print 'Error fetching data for user'
+    else:
+        user_info = json.loads(info.get_text())
+        return user_info
+
+
+def get_user_info2(user_id):
+    fb_url = "https://www.facebook.com/{0}".format(user_id)
+    print "Processing user id: {0}".format(user_id)
+
+    br.open(fb_url)
+    html = br.response().read()
+    soup = BeautifulSoup(html, 'html.parser')
     info = soup.find(type="application/ld+json")
     if info is None:
         print 'Error fetching data for user'
@@ -48,4 +63,4 @@ def get_event_attendees(event_id):
 
 attendees = get_event_attendees(target_id)
 for attendee in attendees:
-    print get_user_info(attendee['id'])
+    print get_user_info2(attendee['id'])
